fbee39e97e0c85bade53231792f66b86674cea80
celiga <celine.garel@yahoo.fr>
2016-06-12 22:12:05 +0200

    added recent posts in footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  before_action :load_articles
+
+  private
+
+  def load_articles
+    @recent_posts = Post.order("created_at DESC").limit(3)
+  end
 end
